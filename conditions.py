f6c1c61750611b3aedbec8b2c4007f461a966608
Fasika Belayneh <fafibelayneh@gmail.com>
2025-03-30 16:06:57 +0300

    prompted the user to Enter their original price and discount percentage

--- conditions.py
+++ conditions.py
@@ -3,3 +3,5 @@ def calculate_discount(price, discount_percent):
         return price * (1 - discount_percent / 100)
     else:
         return price
+original_price = float(input("Enter the original price of the item: "))
+discount_percent = float(input("Enter the discount percentage: "))
